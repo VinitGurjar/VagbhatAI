16964b41a6af2a1dc17fe01f0ffdf9814091b723
VinitGurjar <vinitfagna@gmail.com>
2024-06-01 13:50:18 +0530

    feat: integrate langchain conversation, chainig and chat history functionality to main app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+from llm_chain import load_normal_chain
+from langchain.memory import StreamlitChatMessageHistory
 
 #for llm chain
-def load_chain():
-    return load_normal_chain()
+def load_chain(chat_history):
+    return load_normal_chain(chat_history)
 
 def clear_input_field():
     st.session_state.user_question = st.session_state.user_input
@@ -12,6 +14,7 @@ def set_send_input():
     st.session_state.send_input = True
     clear_input_field()
 
+#main Client
 def main():
     st.title("VAGBHAT AI")
     chat_container = st.container()
@@ -20,17 +23,22 @@ def main():
         st.session_state.send_input = False
         st.session_state.user_question = ""
 
+    chat_history = StreamlitChatMessageHistory(key="history")
+    llm_chain = load_chain(chat_history)
+
     user_input = st.text_input("Ask Query here", key="user_input", on_change=set_send_input)
 
     send_button = st.button("Send", key="send_button")
 
     if send_button or st.session_state.send_input:
         if st.session_state.user_question != "":
-            llm_response = "This is a response from the LLM"
+
 
             with chat_container:
                 st.chat_message("user").write(st.session_state.user_question)
-                st.chat_message("ai").write("here is an response")
+                llm_response = llm_chain.run(st.session_state.user_question)
+                st.chat_message("ai").write(llm_response)
+                st.session_state.user_question = ""
 
 if __name__ == "__main__":
     main()
